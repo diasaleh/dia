3e43f6fbc5f4e45c8c1ac2d2b2458b277d0651a7
diasaleh <dia-20111@hotmail.com>
2015-12-31 19:54:43 +0200

    create dddd.js

---
+++ dddd.js
@@ -0,0 +1,8 @@
+KEY_STATUS = { keyDown:false };
+for (code in KEY_CODES) {
+  KEY_STATUS[KEY_CODES[code]] = false;
+}
+KEY_STATUS = { keyDown:false };
+for (code in KEY_CODES) {
+  KEY_STATUS[KEY_CODES[code]] = false;
+}
